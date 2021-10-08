529cc69f7b242b80c7a4f5bb4a6b6a917744dfff
Ryan Conklin <ryan.conklin@thinkshout.com>
2021-10-08 12:09:06 -0700

    feat: add media queries and typography tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 const tokens = require("./tokens/tailwind-tokens.json");
 
+const { NODE_ENV = 'production' } = process.env;
+
 module.exports = {
-  mode: "jit",
+  mode: NODE_ENV !== 'production' && 'jit',
   purge: ["./includes/**/*.inc", "./templates/**/*.{html,twig}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: tokens.color,
+    container: {
+      center: true
+    },
     fontFamily: tokens.font.family,
+    fontSize: tokens.font.size,
     fontWeight: tokens.font.weight,
     gap: tokens.grid.gap,
+    lineHeight: tokens.lineHeight,
     opacity: tokens.opacity,
     screens: tokens.screen,
     spacing: tokens.spacer,
